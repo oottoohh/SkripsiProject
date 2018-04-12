f3b8ebdd36df88f27495561203a7c609570ea861
Ottoh Hidayatullah <ottohhidayatullah@gmail.com>
2018-04-12 18:46:28 +0700

    12April 2018
    Adding Stemming Stop Word

--- Code/Modeling/ProjectClassific/ClassVar.py
+++ Code/Modeling/ProjectClassific/ClassVar.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
 from pandas.io.json import json_normalize
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from StopWordID import get_stop
+from sklearn.feature_extraction.text import TfidfVectorizer
 def AccesDb():
     client = MongoClient()
     client = MongoClient('localhost', 27017)
@@ -8,3 +11,16 @@ def AccesDb():
     ListData = (list(dt.find()))
     df = json_normalize(ListData)
     return df
+
+def Stemming(df):
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    df = df.SkripsiTitle.astype(str).str.split(',')
+    df["SkripsiTitle"] = df.apply(lambda x: [stemmer.stem(y) for y in x])
+    df["SkripsiTitle"] = df['SkripsiTitle'].apply(lambda x : " ".join(x))
+    return df
+
+def StopWord():
+    IDStopWords = set(get_stop())
+    tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words=IDStopWords)
+    return tfidf
--- Code/Modeling/ProjectClassific/main.py
+++ Code/Modeling/ProjectClassific/main.py
@@ -2,7 +2,6 @@ from io import StringIO
 from sklearn import svm
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
@@ -16,41 +15,43 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix
 from sklearn.utils import shuffle
 import numpy as np
-from StopWordID import get_stop
 import matplotlib.pyplot as plt
 import pprint
 import pandas as pd
 import seaborn as sns
 import timeit
-from ClassVar import AccesDb
+from ClassVar import AccesDb, Stemming, StopWord
 
 #fig = plt.figure(figsize=(8, 6))
-# IDStopWords = set(get_stop())
-# tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words=IDStopWords)
-# features = tfidf.fit_transform(df.SkripsiTitle).toarray()
-# labels = df.category_id
 
 def main():
     df = AccesDb()
-    Preprocessing(df)
     #CheckData(df)
+    Preprocessing(df)
+    
     
 
 def CheckData(data):
     #print(data.head())
     print ('Jumlah Dataset : ', len(data.index))
-    print('Jumlah Data Skripsi Title', len(data['SkripsiTitle'].value_counts()))
-    print('Jumlah Data Skripsi Abstrak', len(data['SkripsiAbstrak'].value_counts()))
+    print('Jumlah Data Skripsi Title :', len(data['SkripsiTitle'].value_counts()))
+    print('Jumlah Data Skripsi Abstrak :', len(data['SkripsiAbstrak'].value_counts()))
+    print('Jumlah Data Category :\n', data.SkripsiCategory.value_counts())
     data.groupby('SkripsiCategory').SkripsiTitle.count().plot.bar(ylim = 0)
     plt.show()
 
 def Preprocessing(data):
-    #Create category ID
+    df = Stemming(data)
+    print(' ----- Hasil Stemming ----- \n ', df['SkripsiTitle'].head())
     df = CategoryID(data)
     category_id_df = df[['SkripsiCategory', 'category_id']].drop_duplicates().sort_values('category_id')
     category_to_id = dict(category_id_df.values)
     id_to_category = dict(category_id_df[['category_id', 'SkripsiCategory']].values)
     print(df.head())
+    tfidf = StopWord()
+    features = tfidf.fit_transform(df['SkripsiTitle']).toarray()
+    labels = df.category_id
+    print(features.shape)
 
 def CategoryID(data):
     col = ['SkripsiCategory', 'SkripsiTitle']
