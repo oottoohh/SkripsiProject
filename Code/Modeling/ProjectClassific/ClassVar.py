2a5a3a804eb2ecaa88ae4808b1bae0c6e152954c
Ottoh Hidayatullah <ottohhidayatullah@gmail.com>
2018-04-16 12:41:25 +0700

    16 April 2018
    Finished Build Model

--- Code/Modeling/ProjectClassific/ClassVar.py
+++ Code/Modeling/ProjectClassific/ClassVar.py
@@ -3,6 +3,11 @@ from pandas.io.json import json_normalize
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from StopWordID import get_stop
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import StringIO
+
 def AccesDb():
     client = MongoClient()
     client = MongoClient('localhost', 27017)
@@ -13,14 +18,31 @@ def AccesDb():
     return df
 
 def Stemming(df):
+    df = CategoryID(df)
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
-    df = df.SkripsiTitle.astype(str).str.split(',')
-    df["SkripsiTitle"] = df.apply(lambda x: [stemmer.stem(y) for y in x])
-    df["SkripsiTitle"] = df['SkripsiTitle'].apply(lambda x : " ".join(x))
+    df['Tokenized'] = df['SkripsiTitle'].apply(lambda x : filter(None,x.split(" ")))
+    df['TokenizeData'] = df['Tokenized'].apply(lambda x: [stemmer.stem(y) for y in x])
+    df['SkripsiTitle'] = df['TokenizeData'].apply(lambda x : " ".join(x))
     return df
 
-def StopWord():
+def TfidfStopWord():
     IDStopWords = set(get_stop())
-    tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words=IDStopWords)
+    tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words=IDStopWords) 
     return tfidf
+
+def CategoryID(data):
+    col = ['SkripsiCategory', 'SkripsiTitle']
+    df = data[col]
+    df.columns = ['SkripsiCategory', 'SkripsiTitle']
+    df['category_id'] = df['SkripsiCategory'].factorize()[0]
+    return df
+
+def ConfusionMatrix(Ytest, y_pred, df):
+    df = CategoryID(df)
+    category_id_df = df[['SkripsiCategory', 'category_id']].drop_duplicates().sort_values('category_id')
+    conf_mat = confusion_matrix(Ytest, y_pred)
+    sns.heatmap(conf_mat, annot=True, fmt='d', xticklabels=category_id_df.SkripsiCategory.values, yticklabels=category_id_df.SkripsiCategory.values )
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show()
--- Code/Modeling/ProjectClassific/main.py
+++ Code/Modeling/ProjectClassific/main.py
@@ -1,3 +1,4 @@
+from ClassVar import AccesDb, Stemming, TfidfStopWord, CategoryID, ConfusionMatrix
 from io import StringIO
 from sklearn import svm
 from sklearn import metrics
@@ -9,10 +10,10 @@ from sklearn.feature_selection import chi2
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix
 from sklearn.utils import shuffle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,16 +21,45 @@ import pprint
 import pandas as pd
 import seaborn as sns
 import timeit
-from ClassVar import AccesDb, Stemming, StopWord
+import time
+
 
 #fig = plt.figure(figsize=(8, 6))
 
 def main():
     df = AccesDb()
+    texts = ["Aplikasi Keamanan Data Multimedia Message Service (MMS) pada Microsoft Office File Memanfaatkan Algoritma Rivest-Shamir Adleman (RSA) dan Blowfish Berbasis Android",
+            "Rancang Bangun Penerima Pemantau Cairan Infus Berbasis Mikrokontroler Menggunakan Media Komputer"]
     #CheckData(df)
-    Preprocessing(df)
+    StartTime = time.time()
+    #SelectionModel(df)
     
+    start = timeit.default_timer()
+    print ('-------- The SVM model will begin the training process--------')    
+    ClfModel, tfidf, id = TrainModel(df)
+    #TrainModel(df)
+    stop = timeit.default_timer()
+    runtime = stop - start
+    print (' ----- The SVM model has finished training in {0} second ----- !'.format(runtime))
     
+    start = timeit.default_timer()
+    print ('-------- Machine try to Learning the Data--------')    
+    BuildClassification(ClfModel, texts, tfidf, id)
+    stop = timeit.default_timer()
+    runtime = stop - start
+    print (' ----- Machine Learning has been Finishied learn Data in {0} second ----- !'.format(runtime))
+    
+    print ('The Script Executed {0} second !'.format(time.time() - StartTime))
+    
+
+def BuildClassification(ClfModel, data, tfidf, id):
+    TextFeature = tfidf.transform(data)
+    print('---------- Disini ---------- \n', TextFeature)
+    predictions = ClfModel.predict(TextFeature)
+    for text, predicted in zip(data, predictions):
+        print('"{}"'.format(text))
+        print(" - DiPrediksi Kategori Skripsi Diatas adalah : '{}'".format(id[predicted]))
+        print("")
 
 def CheckData(data):
     #print(data.head())
@@ -40,26 +70,143 @@ def CheckData(data):
     data.groupby('SkripsiCategory').SkripsiTitle.count().plot.bar(ylim = 0)
     plt.show()
 
-def Preprocessing(data):
-    df = Stemming(data)
-    print(' ----- Hasil Stemming ----- \n ', df['SkripsiTitle'].head())
+
+
+def SelectionFeatures(data):
+    # ----- Stemming --------
+    #df = Preprocessing(data)
+    #------------------------
+
+    #Without Stemming
     df = CategoryID(data)
-    category_id_df = df[['SkripsiCategory', 'category_id']].drop_duplicates().sort_values('category_id')
-    category_to_id = dict(category_id_df.values)
-    id_to_category = dict(category_id_df[['category_id', 'SkripsiCategory']].values)
-    print(df.head())
-    tfidf = StopWord()
+    #TFIDF
+    tfidf = TfidfStopWord()
+    start = timeit.default_timer()
     features = tfidf.fit_transform(df['SkripsiTitle']).toarray()
     labels = df.category_id
-    print(features.shape)
 
-def CategoryID(data):
-    col = ['SkripsiCategory', 'SkripsiTitle']
-    df = data[col]
-    print(df.columns)
-    df.columns = ['SkripsiCategory', 'SkripsiTitle']
-    df['category_id'] = df['SkripsiCategory'].factorize()[0]
+    # Show Tfidf Scores Matrix
+    # featuresName = tfidf.get_feature_names()
+    # indexTitle = [n for n in df['SkripsiTitle']]
+    # rows, cols = features.nonzero()
+    # for row, col in zip(rows, cols):
+    #     print(row, ".--- Hasil TFIDF ---", (featuresName[col], indexTitle[row]), features[row, col])
+    # print(features.shape)
+
+    #Menemukan Korelasi yang berkaitan dengan SkripsiCategory menggunakan Chi Square Unigram Bigram
+    #category_id_df = df[['SkripsiCategory', 'category_id']].drop_duplicates().sort_values('category_id')
+    # category_to_id = dict(category_id_df.values)    
+    # N = 2
+    # for SkripsiCategory, category_id in sorted(category_to_id.items()):
+    #     features_chi2 = chi2(features, labels == category_id)
+    #     indices = np.argsort(features_chi2[0])
+    #     features_names = np.array(tfidf.get_feature_names())[indices]
+    #     unigrams = [v for v in features_names if len(v.split(' ')) == 1]
+    #     bigrams = [v for v in features_names if len(v.split(' ')) == 2]
+    #     print("*'{}'* : ".format(SkripsiCategory))
+    #     print(" . Kata yang Berelasi (unigrams) : \n    . {}".format('\n    .'.join(unigrams[-N:])))
+    #     print(" . Kata yang Berelasi (Bigrams) : \n     . {}".format('\n    .'.join(bigrams[-N:])))
+
+    stop = timeit.default_timer()
+    runtime = stop - start
+    print("Preprocessing and Selection Features  Process Executed in {0} Second !".format(runtime))
+    return features, labels, tfidf
+
+def Preprocessing(data):
+    #Stemming Process
+    df = Stemming(data)
+    start = timeit.default_timer()
+    print(' ----- Hasil Stemming ----- \n ', df.head())
+    stop = timeit.default_timer()
+    runtime = stop - start
+    print("Stemming Process Executed in {0} Second !".format(runtime))
     return df
+
+def SelectionModel(data):
+    features, labels, tfidf = SelectionFeatures(data)
+    Models = [
+        RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
+        LinearSVC(),
+        MultinomialNB(),
+        LogisticRegression(random_state=0),
+    ]
+    CV = 5
+    cv_df = pd.DataFrame(index=range(CV * len(Models)))
+    Entries = []
+    for model in Models:
+        ModelName = model.__class__.__name__
+        AccuracyData = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
+        for foldIDX, accuracy in enumerate(AccuracyData):
+            Entries.append((ModelName, foldIDX, accuracy))
+    cv_df = pd.DataFrame(Entries, columns=['ModelName', 'foldIDX', 'accuracy'])
+    sns.boxplot(x='ModelName', y='accuracy', data=cv_df)
+    sns.stripplot(x='ModelName', y='accuracy', data=cv_df, size=8, jitter=True, edgecolor='gray', linewidth=2)
+    print(cv_df.groupby('ModelName').accuracy.mean()) 
+    plt.show()
+
+def TrainModel(data):
+    df = CategoryID(data)
+    category_id_df = df[['SkripsiCategory', 'category_id']].drop_duplicates().sort_values('category_id')
+    id_to_category = dict(category_id_df[['category_id', 'SkripsiCategory']].values)
+    
+    features, labels, tfidf = SelectionFeatures(data)
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(features, labels, test_size=0.2, random_state=0)
+
+    TuningModel(Xtrain, Ytrain)
+
+    Model = LinearSVC(C=0.9)
+    Model.fit(Xtrain, Ytrain)
+    y_true, y_pred = Ytest, Model.predict(Xtest)
+    #Model.fit(Xtrain, Ytrain)
+    # Ypred = Model.predict(Xtest)
+    # #ConfusionMatrix(Ytest, Ypred, data)
+    ClfModel = Model.fit(features, labels)
+    
+    #Show Top Gram with Model
+    # category_to_id = dict(category_id_df.values)
+    # TopGram(ClfModel, category_to_id, tfidf)
+    
+    print('--------------Specification Model ! --------------\n', ClfModel)
+    print(metrics.classification_report(y_true, y_pred, target_names=df['SkripsiCategory'].unique()))
+    return ClfModel, tfidf, id_to_category
+
+def TopGram(Model, id, tfidf):
+    N = 2
+    for SkripsiCategory, category_id in sorted(id.items()):
+        indices = np.argsort(Model.coef_[category_id])
+        feature_names = np.array(tfidf.get_feature_names())[indices]
+        unigrams = [v for v in reversed(feature_names) if len(v.split(' ')) == 1][:N]
+        bigrams = [v for v in reversed(feature_names) if len(v.split(' ')) == 2][:N]
+        print("# '{}':".format(SkripsiCategory))
+        print("  . Top unigrams:\n       . {}".format('\n       . '.join(unigrams)))
+        print("  . Top bigrams:\n       . {}".format('\n       . '.join(bigrams)))
+
+def TuningModel(X, y):
+    parameters = [{'kernel': ['rbf'],
+               'gamma': [100000, 10000, 1000, 100, 10, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5],
+                'C': [100000, 10000, 1000, 100, 10, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5]},
+               {'kernel': ['linear'], 'C': [1000, 100, 10, 1, 0.9, 0.7, 0.4, 0.3, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5]}
+            ]
+
+
+    print("# Tuning hyper-parameters")
+    print()
+
+    clf = GridSearchCV(svm.SVC(decision_function_shape='ovr'), parameters, cv=5)
+    clf.fit(X, y)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on training set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+            % (mean, std * 2, params))
+    print()
 
 if __name__ == '__main__':
     main()
